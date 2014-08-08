6b93a20106d8ddd515899f2dc987f6152d48d4e2
Mario Hsieh <mariohsieh@gmail.com>
2014-08-08 00:46:57 -0700

    winning hand logic added

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ function Cards() {
 		var suits = ['Club', 'Diamond', 'Heart', 'Spade'];
 		this.deck = [];
 		for (suit in suits) {
-			for (var i=1;i<14;i++)
+			for (var i=2;i<15;i++)
 				this.deck.push({suit: suits[suit], value: i});
 		}
 	}
@@ -59,7 +59,7 @@ function handCalculate(obj) {
 	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 	// check 4-of-a-kind
-	function is4ofaKind() {
+	function isQuads() {
 		for (var i=0;i<2;i++) {
 			if (values[i] == values[i+1] &&
 					values[i+1] == values[i+2] &&
@@ -75,10 +75,10 @@ function handCalculate(obj) {
 		for (var i=0;i<3;i++) {
 			//console.log(values[i], values[i+1], values[i+2]);
 			if (values[i] == values[i+1] && values[i+1] == values[i+2]) {
-				if (values[0] == values[1] || values[3] == values[4])
-					return true;	// full house
+				if (values[0] !== values[1] || values[3] !== values[4])
+					return "trips";	// trips
 				else
-					return "trips"; // trips
+					return true; // full house
 			}
 		}
 		return false;
@@ -109,14 +109,13 @@ function handCalculate(obj) {
 	
 	// check straight
 	function isStraight() {
-		// Ace-high straight check
-		//value = values[4]-values[3]+(values[2]-values[1])-values[0];
+		// low straight check
 		//console.log(value);
-		if (values[0] == 1 &&
-				values[1] == 10 &&
-				values[2] == 11 &&
-				values[3] == 12 &&
-				values[4] == 13)
+		if (values[0] == 2 &&
+				values[1] == 3 &&
+				values[2] == 4 &&
+				values[3] == 5 &&
+				values[4] == 14)
 			return true;
 		else {
 			// loop to check if difference is 1 between cards
@@ -130,26 +129,156 @@ function handCalculate(obj) {
 
 	// return a score for each type of hand	
 	this.getScore = function() {	
-		if (isFlush() && isStraight())
-			return {"score": 9, "type": "straight flush"};	// straight flush
-		else if (is4ofaKind())
-			return {"score": 8, "type": "4 of a kind"};	// 4-of-a-kind
-		else if (isFullHouse() == true)
-			return {"score": 7, "type": "full house"};	// full house
-		else if (isFlush())
-			return {"score": 6, "type": "flush"};	// flush
-		else if (isStraight())
-			return {"score": 5, "type": "straight"};	// straight
-		else if (isFullHouse() == "trips")
-			return {"score": 4, "type": "trips"};	// trips
-		else if (isPair(2))
-			return {"score": 3, "type": "two pair"};	// two pair
-		else if (isPair(1))
-			return {"score": 2, "type": "one pair"};	// one pair
-		else
-			return {"score": 1, "type": "high card"};	// high card
+		if (isFlush() && isStraight()) {
+			score = 9;
+			type = "straight flush";	// straight flush
+		} else if (isQuads()) {
+			score = 8;
+			type = "4 of a kind";	// 4-of-a-kind
+		} else if (isFullHouse() == true) {
+			score = 7;
+			type = "full house";	// full house
+		} else if (isFlush()) {
+			score = 6;
+			type = "flush";	// flush
+		} else if (isStraight()) {
+			score = 5;
+			type = "straight"	// straight
+		} else if (isFullHouse() == "trips") { 
+			score = 4;
+			type = "trips";	// trips
+		} else if (isPair(2)) {
+			score = 3;
+			type = "two pair";	// two pair
+		} else if (isPair(1)) {
+			score = 2;
+			type = "one pair";	// one pair
+		} else {
+			score = 1;
+			type = "high card";	// high card
+		}
 	}
-	return this.getScore();
+	
+	this.getScore();
+	return {"hand": values,"score": score,"type": type};
+}
+
+function compareHands(obj1, obj2) {
+	console.log(obj1, obj2);
+	
+	function compareQuads() {
+		if (obj1.hand[2] > obj2.hand[2])
+			return console.log("player 1 wins");
+		else
+			return console.log("player 2 wins");
+	}
+	
+	// compare trips
+	function compareTrips() {
+		for (var i=4;i>1;i--) {
+			if (obj1.hand[i] == obj1.hand[i-1] &&
+					obj1.hand[i-1] == obj1.hand[i-2])
+				trips1 = obj1.hand[i];
+			if (obj2.hand[i] == obj2.hand[i-1] &&
+					obj2.hand[i-1] == obj2.hand[i-2])
+				trips2 = obj2.hand[i];
+		}
+		if (trips1 > trips2)
+			return console.log("player 1 wins");
+		if (trips2 > trips1)
+			return console.log("player 2 wins");
+		//compareCard(-1);
+	}
+	// compare two pairs
+	function compare2pair() {
+		var pair1 = [];
+		var pair2 = [];
+		
+		for (var i=4;i>0;i--) {
+			if (obj1.hand[i] == obj1.hand[i-1])
+				pair1.push(obj1.hand[i]);
+			if (obj2.hand[i] == obj2.hand[i-1])
+				pair2.push(obj2.hand[i]);
+		}
+		if (pair1[0] < pair2[0])
+			return console.log("player 2 wins");
+		if (pair1[0] > pair2[0])
+			return console.log("player 1 wins");
+		if (pair1[1] < pair2[1])
+			return console.log("player 2 wins");
+		if (pair1[1] > pair2[1])
+			return console.log("player 1 wins");
+		compareCard(-1);
+	}
+	// compare single pair
+	function comparePair() {
+		for (var i=4;i>0;i--) {
+			if (obj1.hand[i] == obj1.hand[i-1])
+				var pair1 = obj1.hand[i];
+			if (obj2.hand[i] == obj2.hand[i-1])
+				var pair2 = obj2.hand[i];
+		}
+		if (pair1 > pair2)
+			return console.log("player 1 wins");
+		if (pair1 < pair2)
+			return console.log("player 2 wins");
+		compareCard(-1);
+	}
+	
+	function compareCard(num) {
+		for (var i=4;i>num;i--) {
+			if (obj1.hand[i] < obj2.hand[i]) {
+				//console.log(obj1.hand[i] , obj2.hand[i])
+				return console.log("player 2 wins");
+			}
+			if (obj1.hand[i] > obj2.hand[i]) {
+				//console.log(obj1.hand[i] , obj2.hand[i])
+				return console.log("player 1 wins");
+			}	
+		}
+		return console.log("tie game");
+	};
+
+	// check for bigger hand, if tie, then go to tie breakers
+	if (obj1.score > obj2.score)
+		console.log("player 1 wins");
+	else if (obj1.score < obj2.score)
+		console.log("player 2 wins");
+	else {
+		//console.log("tie game");
+		// code for tie breaker
+		switch(obj1.score) {
+			case 9:
+				compareCard(3);
+				break;
+			case 8:
+				compareQuads();
+				break;
+			case 7:
+				compareTrips();
+				break;
+			case 6:
+				compareCard(-1);
+				break;
+			case 5:
+				compareCard(3);
+				break;
+			case 4:
+				compareTrips();
+				break;
+			case 3:
+				compare2pair();
+				break;
+			case 2:
+				comparePair();
+				break;
+			case 1:
+				compareCard(-1);
+				break;				
+			default:
+				console.log("tie game");
+		}
+	}
 }
 
 // game logic constructor
@@ -163,25 +292,31 @@ function gameStart() {
 	cards.shuffleDeck();
 	
 	player1.hand = cards.dealHand(5);
-	//player2.hand = cards.dealHand(5);
+	player2.hand = cards.dealHand(5);
 
 /* for testing 
 	player1.hand = [
-		{"suit": "Heart", "value": 8},
-		{"suit": "Heart", "value": 6},
-		{"suit": "Spade", "value": 12},
-		{"suit": "Heart", "value": 4},
-		{"suit": "Heart", "value": 7}
+		{"suit": "Heart", "value": 3},
+		{"suit": "Spade", "value": 3},
+		{"suit": "Spade", "value": 10},
+		{"suit": "Heart", "value": 3},
+		{"suit": "Club", "value": 14}
 	];
-*/	
-	console.log(player1.hand);
+	player2.hand = [
+		{"suit": "Diamond", "value": 5},
+		{"suit": "Heart", "value": 14},
+		{"suit": "Spade", "value": 5},
+		{"suit": "Diamond", "value": 5},
+		{"suit": "Heart", "value": 13}
+	];	 
+*/
+	//console.log(player1.hand);
 	//console.log(player2.hand);
 	
 	player1.score = handCalculate(player1.hand);
-	//player2.getScore = handCalculate(player2.hand);
+	player2.score = handCalculate(player2.hand);
 	
-	console.log(player1.score);
-	//console.log(player2.score);
+	compareHands(player1.score, player2.score);
 }
 
 // start game
